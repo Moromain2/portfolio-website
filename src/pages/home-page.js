29d582ad740d134a00802fd73a8d34c2533cd856
Romain Mosser <mosser.romain@gmail.com>
2020-04-28 22:33:06 +0200

    created a data file for the homepage

--- src/pages/home-page.js
+++ src/pages/home-page.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import Data from '../data/homepage-data.json';
 
 const HomePage = () => {
   return (
     <div className="page-wrapper">
-      <h1>Accueil</h1>
+    {Data.map((data) => {
+      return (
+        <>
+        <section id="hero" key={data.key} >
+          <h1>{data.heroHeader}</h1>
+          <p>{data.heroContent}</p>
+          <a href="#">{data.heroLinkText}</a>
+        </section>
+        <section id="projects" key={data.key} >
+          <h1>{data.projectsHeader}</h1>
+          <a href="#">{data.projectsLinkText}</a>
+        </section>
+        <section id="services" key={data.key} >
+          <h1>{data.servicesHeader}</h1>
+          <a href="#">{data.servicestLinkText}</a>
+        </section>
+        <section id="about" key={data.key} >
+          <h1>{data.aboutHeader}</h1>
+          <a href="#">{data.aboutLinkText}</a>
+        </section>
+        <section id="about" key={data.key} >
+          <h1>{data.testimonialsHeader}</h1>
+        </section>
+        </>
+      )
+    })}
     </div>
   );
 }
